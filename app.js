55819407fe7b1745f532c7919fc494f6762a0144
Kim Pettersen <kim.a.pettersen@gmail.com>
2012-10-12 17:58:38 -0400

    Added mongodb base connection

--- app.js
+++ app.js
@@ -27,6 +27,20 @@ app.configure(function(){
   });
 });
 
+//Database setup
+var mongo = require('mongodb'),
+  Server = mongo.Server,
+  Db = mongo.Db;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('skeleton_db', server);
+
+db.open(function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
+
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
